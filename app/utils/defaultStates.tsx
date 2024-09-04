e9474b6e37de8e9d5ba564045ffd3dca4a1c7eab
“Pigassooo” <1219632665@qq.com>
2024-09-04 20:56:48 +1000

    first commit

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Sydney",
+    country: "AU",
+    state: "New South Wales",
+    lat: -33.8688197,
+    lon: 151.2092955,
+  },
+  {
+    name: "Beijing",
+    country: "CN",
+    state: "Beijing",
+    lat: 39.9042,
+    lon: 116.4074,
+  },
+  {
+    name: "London",
+    country: "GB",
+    state: "England",
+    lat: 51.5074,
+    lon: 0.1278,
+  },
+  {
+    name: "New York",
+    country: "US",
+    state: "New York",
+    lat: 40.7128,
+    lon: -74.006,
+  },
+  {
+    name: "Barcelona",
+    country: "ES",
+    state: "Catalonia",
+    lat: 41.3828939,
+    lon: 2.1774322,
+  },
+];
+
+export default defaultStates;
